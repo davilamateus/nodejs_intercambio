32cd5da89427b7c265ea21e6e44a00af26fef950
Mateus Davilagit config --global user.email davila_mateus@hotmail.com <davila_mateus@hotmail.com>
2022-10-11 19:21:32 +0100

    Table 0.2

--- controllers/users/ForgetPassword.js
+++ controllers/users/ForgetPassword.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const Users = require('../../models/users/Users')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken');
 const JWTsecret = require('../../middleware/JWTsecret');
 const nodemailer = require("nodemailer"); // Require the Nodemailer package
