659fa799c8d283f61c13d463cd9a96173198b86a
Mateus Davilagit config --global user.email davila_mateus@hotmail.com <davila_mateus@hotmail.com>
2022-10-13 17:23:19 +0100

    vs06

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const CountryController = require('./controllers/citys/Country');
 const CitiesController = require('./controllers/citys/CityController');
 const CommercialUserController = require('./controllers/users/Commercial');
 
-app.use(cors())
+app.use(cors());
 app.use(bodyPaser.json());
 app.use(bodyPaser.urlencoded({extended:false}))
 
@@ -54,24 +54,6 @@ connection
 
 
 
-        app.use(function (req, res, next) {
-
-            // Website you wish to allow to connect
-            res.setHeader('Access-Control-Allow-Origin', "*");
-        
-            // Request methods you wish to allow
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        
-            // Request headers you wish to allow
-            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-        
-            // Set to true if you need the website to include cookies in the requests sent
-            // to the API (e.g. in case you use sessions)
-            res.setHeader('Access-Control-Allow-Credentials', true);
-        
-            // Pass to next layer of middleware
-            next();
-        });
 
 app.use('/', UsersController)
 app.use('/', ToDoListController)
