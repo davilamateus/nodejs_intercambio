e2f08cd99a6b7cfcd2c330d061d4f9a5f6dfe828
Mateus Davilagit config --global user.email davila_mateus@hotmail.com <davila_mateus@hotmail.com>
2022-10-13 17:24:58 +0100

    vs06

--- server.js
+++ server.js
@@ -38,6 +38,14 @@ const CitiesController = require('./controllers/citys/CityController');
 const CommercialUserController = require('./controllers/users/Commercial');
 
 app.use(cors());
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:4200');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 app.use(bodyPaser.json());
 app.use(bodyPaser.urlencoded({extended:false}))
 
